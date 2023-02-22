5a99e75f6ab6c519a11c7284edc5e2dea596facc
Felipe Steffan <felip.developer@hotmail.com>
2023-02-21 21:47:27 -0300

    Entity, repository

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex','alex@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Felipe','felipe@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria','maria@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
